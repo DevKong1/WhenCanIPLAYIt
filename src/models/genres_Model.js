3f45dbfdbf1d7758e435e9dc147a38406bc6abec
Konrad Gomulka <konrad.gomulka@studio.unibo.it>
2020-10-09 23:11:20 +0200

    Remove old files

--- src/models/genres_Model.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(mongoose, mongoosePaginate) {
-    var Schema = mongoose.Schema;
-    var genres_schema = new Schema({
-        name:  {type: String, required: true},
-        code: {type: String, required: true, dropDups: true}
-    });
-    genres_schema.plugin(mongoosePaginate);
-    return mongoose.model('genres_model', genres_schema, 'genres');
-};
--- src/models/platforms_Model.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(mongoose, mongoosePaginate) {
-    var Schema = mongoose.Schema;
-    var platform_schema = new Schema({
-        name:  {type: String, required: true},
-        code: {type: String, required: true, dropDups: true}
-    });
-    platform_schema.plugin(mongoosePaginate);
-    return mongoose.model('platform_model', platform_schema, 'platforms');
-};
