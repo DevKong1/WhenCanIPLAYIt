6a10f198d37e52ce5caf89f3899aa432d07d1981
Konrad Gomulka <konrad.gomulka@studio.unibo.it>
2020-10-22 16:15:46 +0200

    Remove redundant files

--- Backend/ApiService/routes/notification_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const notificationsController = require("../controllers/NotificationsController");
-const { requireAuth } = require("../auth/auth");
-
-router
-    .put("/mail", requireAuth, notificationsController.setMail);
-    
-module.exports = router;
--- dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:latest
-RUN mkdir -p /app
-WORKDIR /app
-COPY . /app
-RUN npm install --unsafe-perm
-EXPOSE 3000 3030
-CMD npm run deploy
